44efa7ccc5c646d444ae155a1b6ffff561f28d9b
Ian MacDonald & David Abel <student@epicodus.com>
2015-04-20 15:46:55 -0700

    queen_attack method passes all specs

--- lib/queen_attack.rb
+++ lib/queen_attack.rb
@@ -1,5 +1,16 @@
 class Array
-  define_method(:queen_attack?) do
-    
+  define_method(:queen_attack?) do |opponent|
+    queen = self
+    if queen[1].eql?(opponent[1])
+      true
+    elsif queen[0].eql?(opponent[0])
+      true
+    elsif queen[0].-(queen[1]).abs().eql?(opponent[0].-(opponent[1]).abs())
+      true
+    elsif queen[0].+(queen[1]).eql?(opponent[0].+(opponent[1]))
+      true
+    else
+      false
+    end
   end
 end
--- spec/queen_attack_spec.rb
+++ spec/queen_attack_spec.rb
@@ -2,7 +2,19 @@ require('rspec')
 require('queen_attack')
 
 describe('Array#queen_attack?') do
-  it('if opponet piece is horizontal to queen, queen can attack') do
-    expect([1, 1].queen_attack?([6, 1])).to(eq(true))
+  it('if opponent piece is horizontal to queen, queen can attack') do
+    expect([1, 2].queen_attack?([6, 2])).to(eq(true))
+  end
+  it('if opponent piece is vertical to queen, queen can attack') do
+    expect([3, 4].queen_attack?([3, 2])).to(eq(true))
+  end
+  it('if opponent piece is diagonal to queen, queen can attack') do
+    expect([1, 1].queen_attack?([2, 2])).to(eq(true))
+  end
+  it('if opponent piece is diagonal to queen, queen can attack') do
+    expect([2, 3].queen_attack?([4, 1])).to(eq(true))
+  end
+  it('if opponent piece cannot be attacked by queen, return false') do
+    expect([2, 3].queen_attack?([5, 1])).to(eq(false))
   end
 end
